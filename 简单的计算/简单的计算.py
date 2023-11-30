9bb2f2d51d30ac3ee2e0da9437b10733ed6acd6b
wenheluanwu <116621826+wenheluanwu@users.noreply.github.com>
2023-11-30 11:52:31 +0800

    Update 简单的计算.py

--- 简单的计算/简单的计算.py
+++ 简单的计算/简单的计算.py
@@ -2,7 +2,6 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
 
 #  把浏览器驱动写入到环境配置
 os.environ["PATH"] += r"D:\ChromeDriver"
